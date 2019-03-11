e97b6dca097d6fa681503a0bb0577aefe4baffa8
Natasha Chernis <natashachernis@gmail.com>
2019-03-11 01:01:05 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    keys = []
+    arguments.map {|arg|
+      self.each {|key, value|
+        #binding.pry
+        keys << key if value == arg}
+    }
+    keys
+
   end
 end
