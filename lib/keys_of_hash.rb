b619edd9ded384c1f5588113175e6e35e7041f80
Natasha Chernis <natashachernis@gmail.com>
2019-03-11 00:48:31 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,10 +2,11 @@ require "pry"
 
 class Hash
   def keys_of(*arguments)
-    binding.pry
+    
     arguments.map {|arg|
-
       self.find {|key, value| value == arg}.first
     }
+    binding.pry
+    
   end
 end
